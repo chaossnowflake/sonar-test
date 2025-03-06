bb507f9ec4f25adb32fb725cf50d02bd933e4136
Anton De Silva <chaossnowflake@gmail.com>
2025-03-06 11:19:07 -0500

    latest

--- src/main/java/org/sonar/Application.java
+++ src/main/java/org/sonar/Application.java
@@ -4,5 +4,7 @@ public class Application {
   public static void main(String[] args) {
     System.out.println("My Application");
     System.out.println("look ma, no logger!");
+
+    int used = 12;
   }
 }
