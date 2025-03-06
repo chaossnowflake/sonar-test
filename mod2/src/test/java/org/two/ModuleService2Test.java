bfb7bcc97720ec4d4b63b369f3eb371bbe5ce659
Anton De Silva <chaossnowflake@gmail.com>
2025-03-06 12:48:38 -0500

    test

---
+++ mod2/src/test/java/org/two/ModuleService2Test.java
@@ -0,0 +1,13 @@
+package org.two;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModuleService2Test {
+
+  @Test
+  void doNothing() {
+    assertEquals(1, 1);
+  }
+}
