6ff5a2d6069b951bbb6634c144dd115ab011bd57
Anton De Silva <chaossnowflake@gmail.com>
2025-03-05 23:11:54 -0500

    loger

--- src/main/java/org/sonar/Application.java
+++ src/main/java/org/sonar/Application.java
@@ -3,5 +3,6 @@ package org.sonar;
 public class Application {
   public static void main(String[] args) {
     System.out.println("My Application");
+    System.out.println("look ma, no logger!");
   }
 }
